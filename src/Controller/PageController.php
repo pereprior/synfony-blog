b2aab5ec52e0253cbf7385d63563eddbd48be217
Pere Prior <pj.prior.es@gmail.com>
2024-10-16 11:48:10 +0200

    Initial commit

---
+++ src/Controller/PageController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Contact;
+use App\Form\ContactFormType;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PageController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('page/index.html.twig', []);
+    }
+
+    #[Route('/about', name: 'about')]
+    public function about(): Response
+    {
+        return $this->render('page/about.html.twig', []);
+    }
+
+    #[Route('/contact', name: 'contact')]
+    public function contact(ManagerRegistry $doctrine, Request $request): Response
+    {
+        $contact = new Contact();
+        $form = $this->createForm(ContactFormType::class, $contact);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contacto = $form->getData();
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($contacto);
+            $entityManager->flush();
+            return $this->redirectToRoute('index', []);
+        }
+        return $this->render('page/contact.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+
+    #[Route('/blog', name: 'blog')]
+    public function blog(): Response
+    {
+        return $this->render('page/blog.html.twig', []);
+    }
+
+    #[Route('/single_post', name: 'single_post')]
+    public function single_post(): Response
+    {
+        return $this->render('page/single_post.html.twig', []);
+    }
+}
