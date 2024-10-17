e44becbe715ea298670119b53a7ef73f7e130891
Pere Prior <pj.prior.es@gmail.com>
2024-10-17 09:32:49 +0200

    Ejercicio 4 finalizado

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use App\Entity\Image;
+use App\Form\CategoryFormType;
+use App\Form\ImageFormType;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class AdminController extends AbstractController
+{
+    #[Route('/admin/images', name: 'app_images')]
+    public function images(ManagerRegistry $doctrine, Request $request, SluggerInterface $slugger): Response
+    {
+        $image = new Image();
+        $form = $this->createForm(ImageFormType::class, $image);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $file = $form->get('file')->getData();
+            if ($file) {
+                $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$file->guessExtension();
+
+                // Move the file to the directory where images are stored
+                try {
+
+                    $file->move(
+                        $this->getParameter('images_directory'), $newFilename
+                    );
+                    $filesystem = new Filesystem();
+                    $filesystem->copy(
+                        $this->getParameter('images_directory') . '/'. $newFilename,
+                        $this->getParameter('portfolio_directory') . '/'.  $newFilename, true);
+
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+
+                // updates the 'file$filename' property to store the PDF file name
+                // instead of its contents
+                $image->setFile($newFilename);
+            }
+            $image = $form->getData();
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($image);
+            $entityManager->flush();
+        }
+
+        $repository = $doctrine->getRepository(Image::class);
+        $images = $repository->findAll();
+        return $this->render('admin/images.html.twig', array(
+            'form' => $form->createView(),
+            'images' => $images
+        ));
+    }
+
+    #[Route('/admin/categories', name: 'app_categories')]
+    public function categories(ManagerRegistry $doctrine, Request $request): Response
+    {
+        $repository = $doctrine->getRepository(Category::class);
+
+        $categories = $repository->findAll();
+
+        $category = new Category();
+        $form = $this->createForm(CategoryFormType::class, $category);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $category = $form->getData();
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($category);
+            $entityManager->flush();
+        }
+        return $this->render('admin/categories.html.twig', array(
+            'form' => $form->createView(),
+            'categories' => $categories
+        ));
+
+    }
+
+
+
+    public function adminDashboard(): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        // or add an optional message - seen by developers
+        $this->denyAccessUnlessGranted('ROLE_ADMIN', null, 'User tried to access a page without having ROLE_ADMIN');
+
+        return new Response("Sí que puedes entrar");
+    }
+}
--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
 use App\Entity\Contact;
 use App\Form\ContactFormType;
 use Doctrine\Persistence\ManagerRegistry;
@@ -13,29 +14,33 @@ use Symfony\Component\Routing\Annotation\Route;
 class PageController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
-        return $this->render('page/index.html.twig', []);
+        $repository = $doctrine->getRepository(Category::class);
+        $categories = $repository->findAll();
+
+        return $this->render('page/index.html.twig', ['categories' => $categories]);
     }
 
     #[Route('/about', name: 'about')]
     public function about(): Response
     {
-        return $this->render('page/about.html.twig', []);
+        return $this->render('page/about.html.twig');
     }
 
     #[Route('/contact', name: 'contact')]
     public function contact(ManagerRegistry $doctrine, Request $request): Response
     {
-        $contact = new Contact();
-        $form = $this->createForm(ContactFormType::class, $contact);
+        $form = $this->createForm(ContactFormType::class, new Contact());
         $form->handleRequest($request);
+
         if ($form->isSubmitted() && $form->isValid()) {
-            $contacto = $form->getData();
+            $contact = $form->getData();
             $entityManager = $doctrine->getManager();
-            $entityManager->persist($contacto);
+            $entityManager->persist($contact);
             $entityManager->flush();
-            return $this->redirectToRoute('contact_sent', []);
+
+            return $this->redirectToRoute('contact_sent');
         }
         return $this->render('page/contact.html.twig', array(
             'form' => $form->createView()
