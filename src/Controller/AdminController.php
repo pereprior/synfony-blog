0b2eda4482e5a2e334fe5a018b341a09457a2ba0
Pere Prior <pj.prior.es@gmail.com>
2024-10-22 11:44:39 +0200

    fin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -12,9 +12,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Filesystem\Filesystem;
-use Symfony\Component\HttpFoundation\File\Exception\FileException;
-use Symfony\Component\String\Slugger\SluggerInterface;
 
 class AdminController extends AbstractController
 {
--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -6,6 +6,7 @@ use App\Entity\Comment;
 use App\Entity\Post;
 use App\Form\CommentFormType;
 use App\Form\PostFormType;
+use App\Repository\PostRepository;
 use App\Service\FileService;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -16,46 +17,25 @@ use Symfony\Component\String\Slugger\SluggerInterface;
 
 class BlogController extends AbstractController
 {
+    private PostRepository $repository;
+    private array $posts;
+    private array $lastFivePosts;
+
+    public function __construct(ManagerRegistry $doctrine)
+    {
+        $repository = $doctrine->getRepository(Post::class);
+
+        $this->repository = $repository;
+        $this->posts = $repository->findAll();
+        $this->lastFivePosts = $repository->findLastPosts();
+    }
+
     #[Route('/blog', name: 'blog')]
     public function blog(ManagerRegistry $doctrine): Response
     {
-        $repository = $doctrine->getRepository(Post::class);
-        $posts = $repository->findAll();
-        $last = $repository->findLastPosts();
-
         return $this->render('blog/index.html.twig', [
-            'posts' => $posts,
-            'recents' => $last
-        ]);
-    }
-
-    #[Route('/single_post/{slug}', name: 'single_post')]
-    public function post(ManagerRegistry $doctrine, Request $request, $slug): Response
-    {
-        $repository = $doctrine->getRepository(Post::class);
-        $commentRepository = $doctrine->getRepository(Comment::class);
-        $post = $repository->findOneBy(["slug"=>$slug]);
-        $recents = $repository->findLastPosts();
-        $comment = new Comment();
-        $form = $this->createForm(CommentFormType::class, $comment);
-        $form->handleRequest($request);
-        $comments = $commentRepository->findAll();
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $comment = $form->getData();
-            $comment->setPost($post);
-            //Aumentamos en 1 el número de comentarios del post
-            $post->setNumComments($post->getNumComments() + 1);
-            $entityManager = $doctrine->getManager();
-            $entityManager->persist($comment);
-            $entityManager->flush();
-            return $this->redirectToRoute('single_post', ["slug" => $post->getSlug()]);
-        }
-        return $this->render('blog/single_post.html.twig', [
-            'post' => $post,
-            'recents' => $recents,
-            'commentForm' => $form->createView(),
-            'comments' => $comments
+            'posts' => $this->posts,
+            'recents' => $this->lastFivePosts
         ]);
     }
 
@@ -98,11 +78,40 @@ class BlogController extends AbstractController
         ));
     }
 
+    #[Route('/single_post/{slug}', name: 'single_post')]
+    public function post(ManagerRegistry $doctrine, Request $request, $slug): Response
+    {
+        $post = $this->repository->findOneBy(["slug"=>$slug]);
+
+        $commentRepository = $doctrine->getRepository(Comment::class);
+        $comment = new Comment();
+        $form = $this->createForm(CommentFormType::class, $comment);
+        $form->handleRequest($request);
+        $comments = $commentRepository->findAll();
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment = $form->getData();
+            $comment->setPost($post);
+            //Aumentamos en 1 el número de comentarios del post
+            $post->setNumComments($post->getNumComments() + 1);
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($comment);
+            $entityManager->flush();
+            return $this->redirectToRoute('single_post', ["slug" => $post->getSlug()]);
+        }
+
+        return $this->render('blog/single_post.html.twig', [
+            'post' => $post,
+            'recents' => $this->lastFivePosts,
+            'commentForm' => $form->createView(),
+            'comments' => $comments
+        ]);
+    }
+
     #[Route('/single_post/{slug}/like', name: 'post_like')]
     public function like(ManagerRegistry $doctrine, $slug): Response
     {
-        $repository = $doctrine->getRepository(Post::class);
-        $post = $repository->findOneBy(["slug"=>$slug]);
+        $post = $this->repository->findOneBy(["slug"=>$slug]);
 
         if ($post){
             $post->like();
@@ -115,15 +124,14 @@ class BlogController extends AbstractController
     }
 
     #[Route('/blog/buscar', name: 'blog_buscar')]
-    public function buscar(ManagerRegistry $doctrine,  Request $request): Response
+    public function buscar(ManagerRegistry $doctrine, Request $request): Response
     {
-        $repository = $doctrine->getRepository(Post::class);
         $searchTerm = $request->query->get('searchTerm', '');
-        $posts = $repository->findByText($searchTerm);
-        $recents = $repository->findLastPosts();
+        $posts = $this->repository->findByText($searchTerm);
+
         return $this->render('blog/index.html.twig', [
             'posts' => $posts,
-            'recents' => $recents,
+            'recents' => $this->lastFivePosts,
             'searchTerm' => $searchTerm
         ]);
     }
